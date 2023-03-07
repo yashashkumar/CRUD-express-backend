6bdca35364489f39d851a97b3bed530ee25c66d1
yashashk <yashashk@sanketika.in>
2023-03-07 12:56:35 +0530

    newConnection.ts added new db connection

---
+++ newConnection.ts
@@ -0,0 +1,11 @@
+const {Client} = require('pg');
+
+const datasetsDB = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "Yashas@200116",
+    database: "dataset"
+})
+
+module.exports = datasetsDB;

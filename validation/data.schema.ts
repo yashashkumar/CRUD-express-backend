e1a978f6d1d870698c08ade6164c4df170e1df73
yashashk <yashashk@sanketika.in>
2023-03-08 23:48:31 +0530

    done with validation of data coming from req.body

---
+++ validation/data.schema.ts
@@ -0,0 +1,14 @@
+const joi = require("joi");
+
+const data = {
+    postData : joi.object({
+        id:joi.string(),
+        dataschema : joi.object(),
+        routerconfig : joi.object(),
+        status : joi.string(),
+        createdBy : joi.string(),
+        updatedBy : joi.string()
+    })
+}
+
+module.exports = data;
---
+++ validation/data.validation.ts
@@ -0,0 +1,16 @@
+const {postData} = require("./data.schema");
+
+module.exports = {
+    addDataValidation : (req:any ,res:any,next:any)=>{
+        let validation = postData.validate(req.body);
+        if(validation.error){
+            res.json({
+                error : "unable to post data",
+                message : validation.error.details[0].message
+            })
+        }
+        else{
+            next();
+        }
+    }
+}

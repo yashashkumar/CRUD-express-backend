400b8132d8507b92b8c52c050db0e72304ed0770
yashashk <yashashk@sanketika.in>
2023-03-31 17:49:39 +0530

    unit test

---
+++ mockunittesting/mocktest.spec.ts
@@ -0,0 +1,173 @@
+// import { getEachRecords } from ".";
+
+import * as db from "../routeImplementation/routeimpl";
+
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+import app from "../server";
+let spies = require("chai-spies");
+chai.use(spies);
+chai.use(chaiHttp);
+chai.should();
+// import { should } from "chai";
+let expect = chai.expect;
+
+describe("get records", () => {
+  it("should GET all the records", (done) => {
+    chai.spy.on(db.pool, "query", () => {
+      return { rows: [{}] };
+    });
+    chai
+      .request(app)
+      .get("/datasets/get")
+      .end((err: any, response: any) => {
+        // if (err) {
+        //   response.should.have.status(500);
+        // }
+        console.log(response.body);
+        response.should.have.status(200);
+        response.body.should.be.a("array");
+        response.body.length.should.not.be.eql(0);
+        chai.spy.restore(db.pool, "query");
+        done();
+      });
+  });
+});
+
+describe("get records by id", () => {
+  it("should GET the records by id", (done) => {
+    chai.spy.on(db.pool, "query", () => {
+      return { rows: [{}] };
+    });
+    chai
+      .request(app)
+      .get("/datasets/getrecord/db12")
+      .end((err: any, response: any) => {
+        // if (err) {
+        //   expect(response.body).to.be.an("strng");
+        // }
+        expect(response.status).to.equal(200);
+        expect(response.body).to.be.an("object");
+        chai.spy.restore(db.pool, "query");
+        done();
+      });
+  });
+  it("should give 400 when id not found", (done) => {
+    chai.spy.on(db.pool, "query", () => {
+      return { rowCount: 0 };
+    });
+    chai
+      .request(app)
+      .get("/datasets/getrecord/000")
+      .end((err: any, response: any) => {
+        expect(response.status).to.equal(400);
+        expect(response.body).to.be.an("object");
+        chai.spy.restore(db.pool, "query");
+        done();
+      });
+  });
+});
+
+describe("delete record", () => {
+  it("should delete a record if id is present", (done) => {
+    chai.spy.on(db.pool, "query", () => {
+      return {};
+    });
+
+    chai
+      .request(app)
+      .delete("/datasets/delete/db62")
+      .end((err: any, response: any) => {
+        response.should.have.status(200);
+        expect(response.body).to.be.an("object");
+        chai.spy.restore(db.pool, "query");
+        done();
+      });
+  });
+
+  it("id not found if id is not present", (done) => {
+    chai.spy.on(db.pool, "query", () => {
+      return { rowCount: 0 };
+    });
+    chai
+      .request(app)
+      .delete("/datasets/delete/db62")
+      .end((err: any, response: any) => {
+        // if (err) {
+        //   response.should.have.status(500);
+        // }
+        // console.log(response);
+        response.should.have.status(400);
+        expect(response.body).to.be.an("object");
+        chai.spy.restore(db.pool, "query");
+        done();
+      });
+  });
+});
+
+let record = {
+  id: "xyz",
+  dataschema: {
+    name: "yashash",
+    lname: "kumar",
+  },
+  routerconfig: {
+    "ip address": "192.168.1.88",
+  },
+  status: "inactive",
+  createdBy: "yashas",
+  updatedBy: "yashas",
+};
+
+describe("create new dataset", () => {
+  it("if data not present with id , insert record", (done) => {
+    chai.spy.on(db.pool, "query", () => {
+      return { rowCount: 0 };
+    });
+    chai
+      .request(app)
+      .get("/datasets/getrecord/xyz")
+      .end((err: any, response: any) => {
+        // console.log(response.body);
+        // done();
+        if (response.body.status === 400) {
+          chai
+            .request(app)
+            .post("/datasets/create")
+            .send(record)
+            .end((err: any, response: any) => {
+              // console.log(response.body);
+              expect(response.status).to.be.equal(201);
+              expect(response.body).to.be.an("object");
+              chai.spy.restore(db.pool, "query");
+              done();
+            });
+        }
+        expect(response.status).to.be.equal(400);
+      });
+  });
+  it("should give primary key violation", (done) => {
+    chai.spy.on(db.pool, "query", () => {
+      return { rowCount: 1 };
+    });
+    chai
+      .request(app)
+      .get("/datasets/getrecord/xyz")
+      .end((err: any, response: any) => {
+        console.log(response.status);
+        if (response.body.status != 400) {
+          chai
+            .request(app)
+            .post("/datasets/create")
+            .send(record)
+            .end((err: any, response: any) => {
+              console.log(response.body);
+              expect(response.status).to.be.equal(400);
+              expect(response.body).to.be.an("object");
+              chai.spy.restore(db.pool, "query");
+              done();
+            });
+        }
+      });
+  });
+});
